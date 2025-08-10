829cae087aa55aa8eff465ed84a0653db6024ddc
almar26 <gomezalmar26@gmail.com>
2025-08-10 22:15:23 +0800

    update vuetify theme

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -30,9 +30,22 @@ export default defineNuxtPlugin((nuxtApp) => {
       },
     },
     theme: {
-      defaultTheme: "customLightTheme",
+      //defaultTheme: "customLightTheme",
       themes: {
-        customLightTheme,
+        // customLightTheme,
+        light: {
+          colors: {
+            primary: '#4CAF50', // Example primary color
+            secondary: '#424242', // Example secondary color
+            accent: '#82B1FF', // Example accent color
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FB8C00',
+            background: '#F5F7F8', // Light background color for the overall page
+            surface: '#FFFFFF' // Card/component background color
+          },
+        },
       },
     },
   });
